9f1090b55ccca0ff93094be67d9bd2c60e636da5
cks3066 <cks3066@naver.com>
2022-07-09 22:07:10 +0900

    fix: 자동 배포시 이미지가 로드 되지 않는 문제

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,22 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-image`,
+    {
+      resolve: "gatsby-remark-copy-linked-files",
+      options: {
+        // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+        // as we assume you'll use gatsby-remark-images to handle
+        // images in markdown as it automatically creates responsive
+        // versions of images.
+        //
+        // If you'd like to not use gatsby-remark-images and just copy your
+        // original images to the public directory, set
+        // `ignoreFileExtensions` to an empty array.
+        destinationDir: "babydragon",
+        ignoreFileExtensions: [],
+      },
+    },
+    `gatsby-plugin-ima,ge`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
