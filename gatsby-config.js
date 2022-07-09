81c5a28355a6201059253a0644d5194cf6c4cc5d
cks3066 <cks3066@naver.com>
2022-07-09 18:06:11 +0900

    feat: 태그용 스키마 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,6 +97,7 @@ module.exports = {
                     frontmatter {
                       title
                       date
+                      tags
                     }
                   }
                 }
