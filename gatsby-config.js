2d9c757c17ab69452ec2be6801b1bc41d56e8f84
intae92 <kangit2013@gmail.com>
2022-07-11 12:43:27 +0900

    chore: rss.xml 파일명 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,7 +104,7 @@ module.exports = {
                 }
               `,
             output: "/rss.xml",
-            title: "Gong Check Team Tech Blog RSS Feed",
+            title: "GongCheck Team Tech Blog RSS Feed",
             match: "^/blog/",
             link: "https://gong-check.github.io/dev-blog/",
           },
